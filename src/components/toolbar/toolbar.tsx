63f5b346ad8d5d9b351b14a8a22b1aed1d30d472
Rolando Andrade <rolandoandradefernandez@gmail.com>
2023-11-11 19:02:05 -0400

    Added animations to the toolbar

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -9,6 +9,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useEditor } from '../editor-context.tsx';
 import { FilenameInput } from './filename-input.tsx';
 import { isFilesystemSupported } from '../../shared/filesystem.ts';
+import { Transition } from '@headlessui/react';
 
 export function Toolbar() {
   const {isRunning, play, stop, showTerminal} = useExecutionContext()
@@ -55,14 +56,56 @@ export function Toolbar() {
   return (
     <div className="flex flex-row justify-between items-center p-1 text-xl px-3 bg-white dark:bg-oneDarkBlack">
       <div className="flex flex-row justify-start items-center" onBlur={hideMenuElements} ref={ref}>
-        <div className="flex flex-row justify-start items-center h-full gap-2">
-          {!showMenu && <ToolbarButton icon={HiOutlineMenu} onClick={showMenuElements} hint={'Menú'}/>}
-          {isFilesystemAPISupported && showMenu && <ToolbarButton icon={MdOutlineSave} onClick={saveCurrentFile} hint={'Guardar'}/>}
-          {isFilesystemAPISupported && showMenu && <ToolbarButton icon={HiPlus} onClick={()=>{}} hint={'Nuevo'}/>}
-          {isFilesystemAPISupported && showMenu && <ToolbarButton icon={MdFolderOpen} onClick={openExistingFile} hint={'Abrir'}/>}
-          {showMenu && <ThemeChanger/>}
-          {!showMenu && <FilenameInput/>}
-        </div>
+        <Transition show={showMenu}>
+            <div className="flex flex-row justify-start items-center h-full gap-2">
+              <Transition.Child
+                enter="transition-opacity duration-500 transition"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0">
+                {isFilesystemAPISupported && <ToolbarButton icon={MdOutlineSave} onClick={saveCurrentFile} hint={'Guardar'}/>}
+              </Transition.Child>
+              <Transition.Child
+                enter="transition-opacity duration-500 transition delay-100"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0">
+                {isFilesystemAPISupported && <ToolbarButton icon={HiPlus} onClick={()=>{}} hint={'Nuevo'}/>}
+              </Transition.Child>
+              <Transition.Child
+                enter="transition-opacity duration-500 transition delay-200"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0">
+                {isFilesystemAPISupported && <ToolbarButton icon={MdFolderOpen} onClick={openExistingFile} hint={'Abrir'}/>}
+              </Transition.Child>
+              <Transition.Child
+                enter="transition-opacity duration-500 transition delay-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0">
+                <ThemeChanger/>
+              </Transition.Child>
+            </div>
+
+          </Transition>
+          <Transition show={!showMenu}
+                      enter="transition-opacity duration-300 transition delay-200"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100">
+            <div className="flex flex-row justify-start items-center h-full gap-2">
+              {!showMenu && <ToolbarButton icon={HiOutlineMenu} onClick={showMenuElements} hint={'Menú'}/>}
+              {!showMenu && <FilenameInput/>}
+            </div>
+          </Transition>
       </div>
       <div className="flex flex-row justify-end items-center">
         <div className="flex flex-row justify-end items-center h-full gap-2">
