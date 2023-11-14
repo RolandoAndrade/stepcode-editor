4ad8018eafc1077f96317bcf9a56d68fd286db00
Rolando Andrade <rolandoandradefernandez@gmail.com>
2023-11-14 14:09:39 -0400

    Move theme change to the context

--- src/components/theme-context.tsx
+++ src/components/theme-context.tsx
@@ -1,18 +1,23 @@
-import { createContext, useContext, useEffect } from 'react';
-import { useLocalStorage } from '@uidotdev/usehooks';
+import { createContext, useContext, useEffect, useRef } from 'react';
+import { useLocalStorage, useWindowSize } from '@uidotdev/usehooks';
+import { toCanvas } from 'html-to-image';
 
 type ThemeContext = {
   theme: string;
   setTheme: (theme: string) => void;
+  play: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 const ThemeContext = createContext<ThemeContext>({
   theme: 'dark',
-  setTheme: () => {}
+  setTheme: () => {},
+  play: () => {},
 });
 
 export function ThemeContextProvider({children}: {children: React.ReactNode}) {
   const [theme, setTheme] = useLocalStorage('theme', 'dark');
+
+  const duration = 500;
 
   function changeTheme(theme: string) {
     setTheme(theme);
@@ -29,8 +34,72 @@ export function ThemeContextProvider({children}: {children: React.ReactNode}) {
     }
   }, [theme])
 
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const {width, height} = useWindowSize();
+
+  async function takeScreenshot(): Promise<HTMLCanvasElement> {
+    return toCanvas(document.getElementsByTagName('body')[0], {
+      width: width || window.innerWidth,
+      height: height || window.innerHeight,
+      canvasWidth: (width || window.innerWidth),
+      canvasHeight: (height || window.innerHeight),
+      pixelRatio: 1,
+      skipAutoScale: true,
+      skipFonts: true,
+    })
+  }
+
+  async function handleChange(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    const canvas = await takeScreenshot();
+    const canvasWrapper = canvasRef.current!;
+    canvasWrapper.style.display = 'block';
+    const { pageX, pageY } = e;
+    const { clientWidth, clientHeight } = document.body;
+    const startDate = Date.now();
+    canvasWrapper.width = width || clientWidth;
+    canvasWrapper.height = height || clientHeight;
+    const ctx = canvasWrapper.getContext("2d")!;
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(canvas, 0, 0)
+    if (theme === 'light') {
+      ctx.globalCompositeOperation = "destination-out";
+    } else {
+      ctx.globalCompositeOperation = "destination-in";
+    }
+    ctx.fillStyle = "white";
+    const finalRadius = Math.sqrt(
+      Math.max(clientWidth * clientWidth,
+        clientHeight * clientHeight)
+    );
+    const render = () => {
+      const diff = Date.now() - startDate;
+      const progress = diff / duration;
+      let radius = 0;
+      if (theme === 'dark') {
+        radius = Math.max(finalRadius * (1 - progress), 0);
+      } else {
+        radius = finalRadius * progress;
+      }
+      ctx.beginPath();
+      ctx.arc(pageX, pageY, radius, 0, 2 * Math.PI);
+      ctx.fill();
+      if (progress < 1) requestAnimationFrame(render);
+      else canvasWrapper.style.display = 'none';
+    };
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+    requestAnimationFrame(render);
+  }
+
   return (
-    <ThemeContext.Provider value={{theme, setTheme: changeTheme}}>
+    <ThemeContext.Provider value={{theme, setTheme: changeTheme, play: handleChange}}>
+      <canvas ref={canvasRef} width={width || '100%'} height={height || '100%'} className={'fixed top-0 left-0'} style={{
+        width: width || '100%',
+        height: height || '100%',
+        zIndex: 1000,
+        display: 'none',
+        pointerEvents: 'none',
+      }}></canvas>
       {children}
     </ThemeContext.Provider>
   );
--- src/components/toolbar/theme-changer.tsx
+++ src/components/toolbar/theme-changer.tsx
@@ -1,86 +1,17 @@
-import { useRef } from 'react';
-import { toCanvas } from 'html-to-image'
 import { ToolbarButton } from './buttons/toolbar-button.tsx';
-import { useTheme } from '../theme-context.tsx';
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
-import { useWindowSize } from '@uidotdev/usehooks';
+import { useTheme } from '../theme-context.tsx';
 
-const duration = 500;
 
 export function ThemeChanger() {
-  const { theme, setTheme } = useTheme();
+  const { theme, play } = useTheme();
 
   function toggleTheme(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    handleChange(e)
+    play(e)
   }
-
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  const {width, height} = useWindowSize();
-
-  async function takeScreenshot(): Promise<HTMLCanvasElement> {
-    return toCanvas(document.getElementsByTagName('body')[0], {
-      width: width || window.innerWidth,
-      height: height || window.innerHeight,
-      canvasWidth: (width || window.innerWidth),
-      canvasHeight: (height || window.innerHeight),
-      pixelRatio: 1,
-      skipAutoScale: true,
-      skipFonts: true,
-    })
-  }
-
-  async function handleChange(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    const canvas = await takeScreenshot();
-    const canvasWrapper = canvasRef.current!;
-    canvasWrapper.style.display = 'block';
-    const { pageX, pageY } = e;
-    const { clientWidth, clientHeight } = document.body;
-    const startDate = Date.now();
-    canvasWrapper.width = width || clientWidth;
-    canvasWrapper.height = height || clientHeight;
-    const ctx = canvasWrapper.getContext("2d")!;
-    ctx.imageSmoothingEnabled = false;
-    ctx.drawImage(canvas, 0, 0)
-    if (theme === 'light') {
-      ctx.globalCompositeOperation = "destination-out";
-    } else {
-      ctx.globalCompositeOperation = "destination-in";
-    }
-    ctx.fillStyle = "white";
-    const finalRadius = Math.sqrt(
-      Math.max(clientWidth * clientWidth,
-        clientHeight * clientHeight)
-    );
-    const render = () => {
-      const diff = Date.now() - startDate;
-      const progress = diff / duration;
-      let radius = 0;
-      if (theme === 'dark') {
-        radius = Math.max(finalRadius * (1 - progress), 0);
-      } else {
-        radius = finalRadius * progress;
-      }
-      ctx.beginPath();
-      ctx.arc(pageX, pageY, radius, 0, 2 * Math.PI);
-      ctx.fill();
-      if (progress < 1) requestAnimationFrame(render);
-      else canvasWrapper.style.display = 'none';
-    };
-    setTheme(theme === 'dark' ? 'light' : 'dark');
-    requestAnimationFrame(render);
-  }
-
 
   return (
     <>
-      <canvas ref={canvasRef} width={width || '100%'} height={height || '100%'} className={'fixed top-0 left-0'} style={{
-        width: width || '100%',
-        height: height || '100%',
-        zIndex: 1000,
-        display: 'none',
-        pointerEvents: 'none',
-      }}></canvas>
       {theme == 'light' ? <ToolbarButton icon={MdDarkMode} onClick={toggleTheme} hint={'Modo oscuro'}/>:
         <ToolbarButton icon={MdLightMode} onClick={toggleTheme} hint={'Modo claro'}/>}
     </>
