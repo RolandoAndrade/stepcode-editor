266dcb8a1fe927b08518e80a267e294729899c6a
Rolando Andrade <rolandoandradefernandez@gmail.com>
2023-11-15 16:18:15 -0400

    Finished legacy completions

--- src/components/editor/codemirror/completions/stepcode.completions.ts
+++ src/components/editor/codemirror/completions/stepcode.completions.ts
@@ -4,6 +4,7 @@ import { structuresCompletions } from './structures.completions.ts';
 import { conditionalsCompletions } from './conditionals.completions.ts';
 import { loopCompletions } from './loop.completions.ts';
 import { definitionCompletions } from './definition.completions.ts';
+import { functionCompletions } from './function.completions.ts';
 
 function langCompletions(context: CompletionContext): CompletionResult | null {
   const word = context.matchBefore(/\w*/)
@@ -15,7 +16,8 @@ function langCompletions(context: CompletionContext): CompletionResult | null {
       ...structuresCompletions,
       ...conditionalsCompletions,
       ...loopCompletions,
-      ...definitionCompletions
+      ...definitionCompletions,
+      ...functionCompletions
     ]
   }
 }
