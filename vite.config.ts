34ca5f9f5ff7a922c8e145e25fb5cf9d22d06e98
Rolando Andrade <rolandoandradefernandez@gmail.com>
2023-11-17 19:37:49 -0400

    Added PWA features

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,54 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWAOptions } from 'vite-plugin-pwa';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  includeAssets: ["favicon.ico", "apple-touch-icon-180x180.png", "maskable_icon-512x512.png", "stepcode.png"],
+  manifest: {
+    name: "StepCode | Editor",
+    short_name: "StepCode",
+    description: "Editor de pseudocódigo",
+    icons: [
+      {
+        src: "/pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#282c34",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(pwaOptions)
+  ],
   worker: {
     format: 'es',
   },
@@ -13,3 +58,4 @@ export default defineConfig({
     ]
   }
 })
+
